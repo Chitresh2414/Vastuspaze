5d6ce522f68cbd6edbf620623e548cec85c17a1d
Chitresh2414 <Chitresharya123@gmail.com>
2025-08-26 00:02:44 +0530

    Enhance contact page layout and styling; integrate Swiper for image slider functionality

--- js/script.js
+++ js/script.js
@@ -3,13 +3,20 @@ const mobileMenu = document.querySelector('.navbar-phone');
 
 toggleBtn.addEventListener('click', () => {
     mobileMenu.classList.toggle('show');
-
-    // Change icon when menu is open/closed
-    if (mobileMenu.classList.contains('show')) {
-        toggleBtn.classList.remove('ri-menu-3-line');
-        toggleBtn.classList.add('ri-close-line');
-    } else {
-        toggleBtn.classList.remove('ri-close-line');
-        toggleBtn.classList.add('ri-menu-3-line');
-    }
+    toggleBtn.classList.toggle('ri-menu-3-line');
+    toggleBtn.classList.toggle('ri-close-line');
 });
+
+ const swiper = new Swiper(".mySwiper", {
+    loop: true,
+    autoplay: {
+      delay: 2000, // 2 seconds
+      disableOnInteraction: false, // keeps autoplay after swipe
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    effect: "slide", // You can change to "fade" for smooth effect
+    speed: 800
+  });
